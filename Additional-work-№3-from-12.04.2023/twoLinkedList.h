21c9127b260950564e7022be7433c3168bc61937
Dmitry Korepin <97905730+DreDDeR85@users.noreply.github.com>
2023-04-12 22:05:23 +0300

    added comments to functions

--- Additional-work-№3-from-12.04.2023/twoLinkedList.h
+++ Additional-work-№3-from-12.04.2023/twoLinkedList.h
@@ -7,12 +7,21 @@
 
 typedef struct TwoLinkedList TwoLinkedList;
 
+//this function creates two-linked list
 TwoLinkedList *creatingTwoLinkedList();
 
+//this function lets you add value
+//to the right side of two-linked list
 int addingElement(TwoLinkedList *twoLinkedList, int value);
 
+//this function erases all units of
+//two-linked list and him itself
 void clearTwoLinkedList(TwoLinkedList *twoLinkedList);
 
+//this function prints on your screen
+//two-linked list
 void printTwoLinkedList(TwoLinkedList *twoLinkedList);
 
+//this function checks if your two-linked list
+//is symmetrical with respect to the center
 bool isTwoLinkedListSymmetric(TwoLinkedList *twoLinkedList);
